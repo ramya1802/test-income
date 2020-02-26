0dff73b8790378ee6183a550747dffb12e397e98
ramya1802 <37345222+ramya1802@users.noreply.github.com>
2020-02-25 22:14:13 -0600

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,16 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-REL_DIR="resource-tutorial"
-
-echo "Automated build option selected, proceeding..."
-
-
-
-TEMP1=`jq -r '.TEMP1' $REL_DIR/test.json`
-TEMP2=`jq -r '.TEMP2' $REL_DIR/test.json`
-
-echo "TEMP1Y=$TEMP1"
-echo "TEMP2=$TEMP2"
-echo "testinggggg"
-
-
+set -e # fail fast
+set -x # print commands
